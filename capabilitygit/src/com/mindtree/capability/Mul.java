9384156b581592fd6e61b16b432bcc6ee14b5e60
Naveenbanka1998 <Naveenbanka1998>
2021-05-19 11:14:29 +0530

    adding file

---
+++ capabilitygit/src/com/mindtree/capability/Mul.java
@@ -0,0 +1,12 @@
+package com.mindtree.capability;
+
+public class Mul {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=4,b=7,c=20;
+		System.out.println("multiplication is:"+(a*b*c));
+
+	}
+
+}
---
+++ capabilitygit/src/com/mindtree/capabilitygit/Addition.java
@@ -0,0 +1,9 @@
+package com.mindtree.capabilitygit;
+
+public class Addition {
+	public static void main(String[] args){
+		int a=12,b=17;
+		System.out.println("the addition is:"+(a+b));
+	}
+
+}
